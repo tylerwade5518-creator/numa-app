b1a07bcfae084266cf1d050667433c112edbffcd
Tyler <YourEmail@whatever.com>
2026-05-26 13:34:20 -0700

    Use implicit auth flow for password recovery

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -4,7 +4,12 @@ import { createBrowserClient } from "@supabase/ssr";
 export function createSupabaseBrowser() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        flowType: "implicit",
+      },
+    }
   );
 }
 
